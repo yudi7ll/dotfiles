b369ad9b7a3f2b8bf187f2166256e2b863c97d14
Yudi <madewahyudi22@gmail.com>
2020-10-06 14:22:54 +0800

    23

---
+++ oh-my-zsh/lib/directories.zsh
@@ -0,0 +1,6 @@
+# List directory contents
+alias lsa='exa -lag --group-directories-first'
+alias l='exa -lag --group-directories-first'
+alias ll='exa -lg --group-directories-first'
+alias la='exa -lAg --group-directories-first'
+alias ldir='exa -lgD'
