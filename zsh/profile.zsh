bab9edf661f67b03ec61c06b13c03f78c075d91a
Yudi <madewahyudi22@gmail.com>
2021-03-15 22:49:01 +0800

    36

--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -1,14 +1,14 @@
-# yarn
+# yarn global
 export PATH=$HOME/.yarn/bin:$PATH
 
 # composer vendor
 export PATH=$HOME/.config/composer/vendor/bin:$PATH
 
-# local x
+# local
 export PATH=$HOME/.local/bin:$PATH
 
 # Flutter
-export PATH=/opt/flutter/bin:$PATH
+# export PATH=/opt/flutter/bin:$PATH
 
 # ruby
 export PATH=$HOME/.gem/ruby/2.7.0/bin:$PATH
