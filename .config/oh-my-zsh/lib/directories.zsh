5a0e8dfb3ee81290880e574c4958cbd04f99f5a6
Yudi <madewahyudi22@gmail.com>
2021-04-08 09:58:38 +0800

    change .config dir to dotconfig

--- .config/oh-my-zsh/lib/directories.zsh
+++
@@ -1,6 +0,0 @@
-# List directory contents
-alias lsa='exa -lagH --group-directories-first --git --icons'
-alias l='exa -lagH --group-directories-first --git --icons'
-alias ll='exa -lgH --group-directories-first --git --icons'
-alias la='exa -lAgH --group-directories-first --git --icons'
-alias ldir='exa -lgDH --git --icons'
